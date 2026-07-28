4fcd0500a10c0687c95523f0d2610e6cec2700cb
Arushi Kumari <arushikumari375@gmail.com>
2026-07-28 17:09:16 +0530

    Create 1646-KthMissingPositiveNumber

---
+++ 1646-KthMissingPositiveNumber/1646-KthMissingPositiveNumber.java
@@ -0,0 +1,18 @@
+// Last updated: 7/28/2026, 5:09:15 PM
+class Solution {
+    public int findKthPositive(int[] arr, int k) {
+        int low=0;
+        int high=arr.length-1;
+        while(low<=high){
+            int mid=low+(high-low)/2;
+            int missingNo=arr[mid]-(mid+1);
+            if(missingNo<k){
+                low=mid+1;
+            }
+            else{
+                high=mid-1;
+            }
+        }
+        return high+k+1;
+    }
+}
