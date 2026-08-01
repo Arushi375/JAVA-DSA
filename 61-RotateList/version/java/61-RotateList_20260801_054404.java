296f2b0470ab61a1eb8d16cd304e447be655c6ed
Arushi Kumari <arushikumari375@gmail.com>
2026-08-01 05:44:04 +0530

    Create 61-RotateList

---
+++ 61-RotateList/version/java/61-RotateList_20260801_054404.java
@@ -0,0 +1,48 @@
+// Last updated: 8/1/2026, 5:44:04 AM
+1/**
+2 * Definition for singly-linked list.
+3 * public class ListNode {
+4 *     int val;
+5 *     ListNode next;
+6 *     ListNode() {}
+7 *     ListNode(int val) { this.val = val; }
+8 *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+9 * }
+10 */
+11class Solution {
+12    public ListNode rotateRight(ListNode head, int k) {
+13        if(head==null||head.next==null||k==0){
+14            return head;
+15        }
+16        ListNode curr=head;
+17        ListNode prev=null;
+18        ListNode newHead;
+19        ListNode tail=head;;
+20        int len=0;
+21        while(tail.next!=null){
+22            tail=tail.next;
+23            len++;
+24        }
+25        len++;
+26        k=k%len;
+27        if(k==0){
+28            return head;
+29        }
+30        curr=head;
+31        while(k>0){
+32            curr=head;
+33            while(curr.next!=null){
+34                prev=curr;
+35                curr=curr.next;
+36            }
+37            tail=curr;
+38            prev.next=null;
+39            tail.next=head;
+40            head=tail;
+41            k--;
+42            
+43        }
+44        return head;
+45        
+46    }
+47}
