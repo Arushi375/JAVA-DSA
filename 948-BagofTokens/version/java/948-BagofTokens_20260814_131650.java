2ab980344e1363d15f3da0dbd20e8edd760e284a
Arushi Kumari <arushikumari375@gmail.com>
2026-08-14 13:16:51 +0530

    Create 948-BagofTokens

---
+++ 948-BagofTokens/version/java/948-BagofTokens_20260814_131650.java
@@ -0,0 +1,30 @@
+// Last updated: 8/14/2026, 1:16:50 PM
+// used greedy + sorting + two pointer approach
+1class Solution {
+2    public int bagOfTokensScore(int[] tokens, int power) {
+3        Arrays.sort(tokens);
+4        int n=tokens.length;
+5        int left=0;
+6        int right=n-1;
+7        int score=0;
+8        int max_score=score;
+9        int i=0;
+10        while(left<=right){
+11            if(power>=tokens[left]){
+12                score+=1;
+13                power-=tokens[left];
+14                left++;
+15                max_score=Math.max(max_score,score);
+16            } 
+17            else if(score>0){ 
+18                score-=1;
+19                power+=tokens[right];
+20                right--;
+21            }
+22            else{
+23                break;
+24            }
+25        }
+26        return max_score;
+27    }
+28}
